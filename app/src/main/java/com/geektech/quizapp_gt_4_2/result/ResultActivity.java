9a0704755ebd1918a1b951bf9fef6501484b006b
User <doni-desing>
2020-01-26 17:15:06 +0300

    I'm make mainFragment with decing and add Quiz, Result RecyclerViewK

--- app/src/main/java/com/geektech/quizapp_gt_4_2/result/ResultActivity.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/result/ResultActivity.java
@@ -1,16 +1,26 @@
 package com.geektech.quizapp_gt_4_2.result;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.geektech.quizapp_gt_4_2.R;
+import com.geektech.quizapp_gt_4_2.result.adapter.ResultAdapter;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private ResultAdapter resultAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        init();
+        resultAdapter = new ResultAdapter();
+    }
+    public void init(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_in_result);
+        recyclerView.setAdapter(resultAdapter);
     }
 }
