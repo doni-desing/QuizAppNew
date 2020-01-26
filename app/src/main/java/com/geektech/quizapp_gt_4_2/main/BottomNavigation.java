0a06eb657bd15448c8419e4861db5ef6e90d8417
User <doni-desing>
2020-01-26 19:10:30 +0300

    zz

---
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/main/BottomNavigation.java
@@ -0,0 +1,15 @@
+package com.geektech.quizapp_gt_4_2.main;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavigation extends BottomNavigationView {
+
+    BottomNavigation bottomNavigation = new Bot
+    public BottomNavigation(@NonNull Context context) {
+        super(context);
+    }
+}
